9051f1747214b057a2162187398d2b2b0fe09e36
Djucbko <109214332+Djucbko@users.noreply.github.com>
2023-03-25 10:17:26 +0000

    Deploying to gh-pages from @ Djucbko/goit-react-hw-05-movies@910f8179ae7f7da74dcc83a59b32cba6586b3cbf 🚀

---
+++ static/js/154.bb275358.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[154],{9154:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var r=t(1087),s="NotFoundPage_wrapper__PyNsQ",a="NotFoundPage_link__iZyh1",o=t(184),c=function(){return(0,o.jsxs)("div",{className:s,children:[(0,o.jsx)("p",{children:"Page not found"}),(0,o.jsxs)("p",{children:["Visit"," ",(0,o.jsxs)(r.rU,{to:"/",className:a,children:[" ","Home page"]})]})]})}}}]);
+//# sourceMappingURL=154.bb275358.chunk.js.map
